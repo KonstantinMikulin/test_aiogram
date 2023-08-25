7e34fd9d29123c630472d36bc32561dab2448008
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-08-25 19:58:15 +0300

    Update to Bot API. Polling.

--- main.py
+++ main.py
@@ -3,30 +3,24 @@ import time
 import config
 
 API_URL = 'https://api.telegram.org/bot'
-API_CATS_URL: str = 'https://api.thecatapi.com/v1/images/search'
 BOT_TOKEN: str = config.TOKEN
-ERROR_TEXT: str = 'Here must be a cat :('
-
 offset: int = -2
-counter: int = 0
-cat_response: requests.Response
-cat_link: str
+updates: dict
 
-while counter < 100:
-    print('attempt= ', counter)
+
+def do_something() -> None:
+    print('Was update')
+
+
+while True:
+    start_time = time.time()
     updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
 
     if updates['result']:
         for result in updates['result']:
             offset = result['update_id']
-            chat_id = result['message']['from']['id']
-            cat_response = requests.get(API_CATS_URL)
+            do_something()
 
-            if cat_response.status_code == 200:
-                cat_link = cat_response.json()[0]['url']
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
-            else:
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
-
-    time.sleep(1)
-    counter += 1
+    time.sleep(3)
+    end_time = time.time()
+    print(f'Update`s time to Telegram Bot API: {end_time - start_time}')
