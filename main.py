4e4d20f50945f6d43b2d0a2b7b050fe525b32ad0
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-08-25 20:04:21 +0300

    Updates from Bot API. Long polling

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import config
 API_URL = 'https://api.telegram.org/bot'
 BOT_TOKEN: str = config.TOKEN
 offset: int = -2
+timeout: int = 10
 updates: dict
 
 
@@ -14,13 +15,12 @@ def do_something() -> None:
 
 while True:
     start_time = time.time()
-    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}&timeout={timeout}').json()
 
     if updates['result']:
         for result in updates['result']:
             offset = result['update_id']
             do_something()
 
-    time.sleep(3)
     end_time = time.time()
     print(f'Update`s time to Telegram Bot API: {end_time - start_time}')
