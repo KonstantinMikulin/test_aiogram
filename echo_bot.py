64c754bc3856ae0314b384de443684d637d4c9b6
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-08-27 18:26:16 +0300

    Simple echo bot

--- echo_bot.py
+++ echo_bot.py
@@ -10,11 +10,23 @@ bot: Bot = Bot(token=API_TOKEN)
 dp: Dispatcher = Dispatcher()
 
 
+# handler for command /start
 @dp.message(Command(commands=['start']))
 async def cmd_start(message: Message):
     await message.answer('Hello! Im the Bot!')
 
 
+# handler for command /help
 @dp.message(Command(commands=['help']))
 async def cmd_help(message: Message):
-    await message.an
+    await message.answer('Send me some and I will echo')
+
+
+# handler for any messages
+@dp.message()
+async def send_echo(message: Message):
+    await message.answer(text=message.text)
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
