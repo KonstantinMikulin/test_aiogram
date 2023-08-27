1f0ed7e1e18a4fe74ce7462151647d357c6fb0e1
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-08-27 18:44:59 +0300

    Simple echo bot with commands registeres

--- echo_bot.py
+++ echo_bot.py
@@ -11,22 +11,23 @@ dp: Dispatcher = Dispatcher()
 
 
 # handler for command /start
-@dp.message(Command(commands=['start']))
 async def cmd_start(message: Message):
     await message.answer('Hello! Im the Bot!')
 
 
 # handler for command /help
-@dp.message(Command(commands=['help']))
 async def cmd_help(message: Message):
     await message.answer('Send me some and I will echo')
 
 
 # handler for any messages
-@dp.message()
 async def send_echo(message: Message):
     await message.answer(text=message.text)
 
+
+dp.message.register(cmd_start, Command(commands=['start']))
+dp.message.register(cmd_help, Command(commands=['help']))
+dp.message.register(send_echo)
 
 if __name__ == '__main__':
     dp.run_polling(bot)
