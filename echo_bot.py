2d46840c52433aab3942b71d32795bf35fc70df1
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-08-28 16:25:05 +0300

    Echo bot with saving photo`s ID into variable

--- echo_bot.py
+++ echo_bot.py
@@ -5,6 +5,7 @@ from aiogram.types import Message
 import config
 
 API_TOKEN: str = config.TOKEN
+photo: str = 'AgACAgIAAxkBAAIJFGTsn1Ol02qYwJWe8W1did2IpwSXAALhzzEbI1FoSz611afRKQt8AQADAgADcwADMAQ'
 
 bot: Bot = Bot(token=API_TOKEN)
 dp: Dispatcher = Dispatcher()
@@ -13,6 +14,8 @@ dp: Dispatcher = Dispatcher()
 # handler for command /start
 @dp.message(Command(commands=['start']))
 async def cmd_start(message: Message):
+    print(message.model_dump_json(indent=4, exclude_none=True))
+
     await message.answer('Hello! Im the Bot!')
 
 
@@ -22,9 +25,27 @@ async def cmd_help(message: Message):
     await message.answer('Send me some and I will echo')
 
 
+# handler for send photo
+@dp.message(Command(commands=['photo']))
+async def send_photo(message: Message):
+    global photo
+    await message.bot.send_photo(chat_id=message.chat.id, photo=photo)
+
+
+@dp.message()
+async def get_photo_id(message: Message):
+    global photo
+
+    photo = message.photo[0].file_id
+    print(photo)
+    await message.answer(text='Photo ID was saved')
+
+
 # handler for any messages
 @dp.message()
 async def send_echo(message: Message):
+    print(message.model_dump_json(indent=4, exclude_none=True))
+
     try:
         await message.send_copy(chat_id=message.chat.id)
     except TypeError:
