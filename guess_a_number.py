7aecdc2084bbd758ee20e58cb1fa7c3249a1ef4e
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-08-28 22:32:18 +0300

    Guess a number bot few more lines

--- guess_a_number.py
+++ guess_a_number.py
@@ -31,7 +31,7 @@ def get_random_number() -> int:
 async def cmd_start(message: Message):
     await message.answer('Привет!\nДавайте сыграем в игру "Угадай число!"?\n\n'
                          'Чтобы получить правила игры и список доступных '
-                         'команд - отправьте команду /help')
+                         'команд - отправьте команду "/help"')
 
 
 # handler for "/help" command
@@ -63,4 +63,54 @@ async def cmd_cancel(message: Message):
 
 
 # handler for processing game start
-@dp.message(F.text.lower().in_(['да', '', '', '']))
+@dp.message(F.text.lower().in_(['да', 'давай', 'сыграем', 'игра', 'играть',
+                                'хочу играть']))
+async def process_positive_answer(message: Message):
+    if not user['in_game']:
+        user['in_game'] = True
+        user['secret_number'] = get_random_number()
+        user['attempts'] = ATTEMPTS
+        await message.answer('Ок!\n\nЯ загадал число от 1 до 100, '
+                             'попробуй угадать!')
+    else:
+        await message.answer('Пока мы играем в игру, я могу '
+                             'реагировать только на числа от 1 до 100 '
+                             'и команды /cancel и /start')
+
+
+# handler for processing negative answer
+@dp.message(F.text.lower().in_['нет', 'не', 'не хочу', 'не буду'])
+async def process_negative_answer(message: Message):
+    if not user['in_game']:
+        await message.answer('Жаль. Если захотите поиграть, напишите')
+    else:
+        await message.answer('Мы же сейчас играем. Присылайте числа от 1 до 100')
+
+
+# handler for processing updates with numbers 1 to 100
+@dp.message(lambda x: x.text and x.text.isdigit() and 1 <= int(x.text) <= 100)
+async def process_numbers_answers(message: Message):
+    if user['in_game']:
+        if int(message.text) == user['secret_number']:
+            user['in_game'] = False
+            user['total_games'] += 1
+            user['wins'] += 1
+            await message.answer('Ура! Вы угадали число! Сыграем еще?')
+        elif int(message.text) > user['secret_number']:
+            user['attempts'] -= 1
+            await message.answer('Мое число меньше')
+        elif int(message.text) < user['secret_number']:
+            await message.answer('Мое число больше')
+
+        if user['attempts'] == 0:
+            user['in_game'] = False
+            user['total_games'] += 1
+            await message.answer(f'''К сожалению у вас больше не осталось 
+                                    попыток. Вы проиграли. Мой число было
+                                    {user['secret_number']}
+                                    Давайте сыграем еще?''')
+    else:
+        await message.answer('Мы еще не играем. Хотите сыграть?')
+
+
+
