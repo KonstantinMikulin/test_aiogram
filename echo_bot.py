15d2e137c1c2829e97482fb63daa32791bd7545e
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-08-28 09:43:46 +0300

    Echo bot with photo echo

--- echo_bot.py
+++ echo_bot.py
@@ -1,6 +1,6 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, ContentType
 
 import config
 
@@ -20,6 +20,12 @@ async def cmd_help(message: Message):
     await message.answer('Send me some and I will echo')
 
 
+# handler for photo echo
+async def send_photo_echo(message: Message):
+    print(message)
+    await message.reply_photo(message.photo[0].file_id)
+
+
 # handler for any messages
 async def send_echo(message: Message):
     await message.answer(text=message.text)
@@ -27,7 +33,9 @@ async def send_echo(message: Message):
 
 dp.message.register(cmd_start, Command(commands=['start']))
 dp.message.register(cmd_help, Command(commands=['help']))
+dp.message.register(send_photo_echo, F.content_type == ContentType.PHOTO)
 dp.message.register(send_echo)
+
 
 if __name__ == '__main__':
     dp.run_polling(bot)
