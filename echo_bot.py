995c7a979e9b1bd36294003df79b9ccf6941d352
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-08-28 16:54:45 +0300

    Voice processing

--- echo_bot.py
+++ echo_bot.py
@@ -1,11 +1,10 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
 from aiogram.types import Message
 
 import config
 
 API_TOKEN: str = config.TOKEN
-photo: str = 'AgACAgIAAxkBAAIJFGTsn1Ol02qYwJWe8W1did2IpwSXAALhzzEbI1FoSz611afRKQt8AQADAgADcwADMAQ'
 
 bot: Bot = Bot(token=API_TOKEN)
 dp: Dispatcher = Dispatcher()
@@ -25,31 +24,12 @@ async def cmd_help(message: Message):
     await message.answer('Send me some and I will echo')
 
 
-# handler for send photo
-@dp.message(Command(commands=['photo']))
-async def send_photo(message: Message):
-    global photo
-    await message.bot.send_photo(chat_id=message.chat.id, photo=photo)
-
-
-@dp.message()
-async def get_photo_id(message: Message):
-    global photo
-
-    photo = message.photo[0].file_id
-    print(photo)
-    await message.answer(text='Photo ID was saved')
-
-
-# handler for any messages
-@dp.message()
-async def send_echo(message: Message):
+# handler for voice message
+@dp.message(F.voice)
+async def process_voice(message: Message):
     print(message.model_dump_json(indent=4, exclude_none=True))
 
-    try:
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        await message.reply(text='Not supported')
+    await message.answer(text='It was voice')
 
 
 if __name__ == '__main__':
