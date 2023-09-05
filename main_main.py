39011da56aab65918dc799021cd584dfe40bedab
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-09-05 15:49:22 +0300

    Add .env file

--- main_main.py
+++ main_main.py
@@ -1,16 +1,7 @@
-print(dir())
+import os
+import dotenv
 
-from functions import get_double_number
-from data import my_dict
-from classes import *
+dotenv.load_dotenv()
 
-print('This is main_main.py file')
-
-new_variable: int = 15
-
-if __name__ == '__main__':
-    print('Код ниже не выполнится, если этот файл будет импортируемым модулем в другой исполняемый файл')
-    print(get_double_number(100))
-    print(my_dict)
-    MyClass()
-    print(dir())
+print(os.getenv('BOT_TOKEN'))
+print(os.getenv('ADMIN_ID'))
