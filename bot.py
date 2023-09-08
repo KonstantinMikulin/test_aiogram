c54ea53e6013535b3cb8a8300e11df0d9c2c6a1d
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-09-08 14:52:23 +0300

    Special buttons test

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart
-from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from aiogram.types import Message, KeyboardButton, KeyboardButtonPollType, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 from config import TOKEN
@@ -11,15 +11,24 @@ bot: Bot = Bot(API_TOKEN)
 dp: Dispatcher = Dispatcher()
 
 kb_builder: ReplyKeyboardBuilder = ReplyKeyboardBuilder()
-buttons: list[KeyboardButton] = [KeyboardButton(text=f'Button {i+1}') for i in range(10)]
-kb_builder.add(*buttons)
-kb_builder.adjust(2, 1, repeat=True)
+
+contact_btn: KeyboardButton = KeyboardButton(text='Send phone number',
+                                             request_contact=True)
+geo_btn: KeyboardButton = KeyboardButton(text='Send location',
+                                         request_location=True)
+poll_btn: KeyboardButton = KeyboardButton(text='Make poll',
+                                          request_poll=KeyboardButtonPollType())
+
+kb_builder.row(contact_btn, geo_btn, poll_btn, width=1)
+
+keyboard: ReplyKeyboardMarkup = kb_builder.as_markup(resize_keyboard=True,
+                                                     one_time_keyboard=True)
 
 
 @dp.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer('Who is worse?',
-                         reply_markup=kb_builder.as_markup(resizw_keyboard=True))
+    await message.answer(text='Special buttons test',
+                         reply_markup=keyboard)
 
 
 if __name__ == '__main__':
