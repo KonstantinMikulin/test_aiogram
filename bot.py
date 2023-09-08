a02ae38edb917f79ee61c37099e3082b3bfee248
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-09-08 12:55:33 +0300

    Some minore changes

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart
 from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 from config import TOKEN
 
@@ -9,40 +10,17 @@ API_TOKEN = TOKEN
 bot: Bot = Bot(API_TOKEN)
 dp: Dispatcher = Dispatcher()
 
-button_1: KeyboardButton = KeyboardButton(text='Dogs 🦮')
-button_2: KeyboardButton = KeyboardButton(text='Pickles 🥒')
-button_3: KeyboardButton = KeyboardButton(text='Ducks?')
-button_4: KeyboardButton = KeyboardButton(text='Cats?')
-
-keyboard_1: ReplyKeyboardMarkup = ReplyKeyboardMarkup(keyboard=[[button_1, button_2], [button_3, button_4]],
-                                                      resize_keyboard=True,
-                                                      one_time_keyboard=True)
+kb_builder: ReplyKeyboardBuilder = ReplyKeyboardBuilder()
+buttons: list[KeyboardButton] = [KeyboardButton(text=f'Button {i+1}') for i in range(10)]
+kb_builder.add(*buttons)
+kb_builder.adjust(2, 1, repeat=True)
 
 
 @dp.message(CommandStart())
 async def cmd_start(message: Message):
     await message.answer('Who is worse?',
-                         reply_markup=keyboard_1)
+                         reply_markup=kb_builder.as_markup(resizw_keyboard=True))
 
-
-@dp.message(F.text == 'Dogs 🦮')
-async def process_dogs_answer(message: Message):
-    await message.answer('Yes! dogs are worst!')
-
-
-@dp.message(F.text == 'Pickles 🥒')
-async def process_pickles_answer(message: Message):
-    await message.answer('Exactly! Pickles are worst!')
-
-
-@dp.message(F.text == 'Ducks?')
-async def process_pickles_answer(message: Message):
-    await message.answer('Ducks???')
-
-
-@dp.message(F.text == 'Cats?')
-async def process_pickles_answer(message: Message):
-    await message.answer('Cats???')
 
 if __name__ == '__main__':
     dp.run_polling(bot)
