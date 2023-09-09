80d2339697458198c310bd7fc46322b7696731cc
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-09-09 23:33:25 +0300

    Some minors

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, KeyboardButton, KeyboardButtonPollType, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from aiogram.types.web_app_info import WebAppInfo
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.methods.delete_message import DeleteMessage
+import time
 
 from config import TOKEN
 
@@ -11,22 +13,13 @@ API_TOKEN = TOKEN
 bot: Bot = Bot(API_TOKEN)
 dp: Dispatcher = Dispatcher()
 
-# Создаем список списков с кнопками
-keyboard: list[list[KeyboardButton]] = [
-    [KeyboardButton(text=str(i)) for i in range(1, 4)],
-    [KeyboardButton(text=str(i)) for i in range(4, 7)],
-    [KeyboardButton(text=str(i)) for i in range(7, 9)]]
-
-# Создаем объект клавиатуры, добавляя в него кнопки
-my_keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(
-    keyboard=keyboard,
-    resize_keyboard=True)
-
 
 @dp.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer(text='Special buttons test',
-                         reply_markup=my_keyboard)
+    msg = await bot.send_sticker(chat_id=message.chat.id,
+                                 sticker='CAACAgIAAxkBAAEKQH5k-489IQABlbQqQgTsMWpjmxRyd_kAAi4PAALNvElJfQjcrqE5xpMwBA')
+    time.sleep(3)
+    await bot.delete_message(chat_id=message.chat.id, message_id=msg.message_id)
 
 
 if __name__ == '__main__':
