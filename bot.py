3929aefa02d4980fba7707b0e6fd70baa3116027
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-09-10 22:54:58 +0300

    Set 'menu' button

--- bot.py
+++ bot.py
@@ -1,26 +1,27 @@
-from aiogram import Bot, Dispatcher, F
-from aiogram.filters import CommandStart, Command
-from aiogram.types import Message, KeyboardButton, KeyboardButtonPollType, ReplyKeyboardMarkup, ReplyKeyboardRemove
-from aiogram.types.web_app_info import WebAppInfo
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
-from aiogram.methods.delete_message import DeleteMessage
-import time
+from aiogram import Bot, Dispatcher
+from aiogram.types import BotCommand
 
-from config import TOKEN
+import config
 
-API_TOKEN = TOKEN
+API_TOKEN: str = config.TOKEN
 
 bot: Bot = Bot(API_TOKEN)
 dp: Dispatcher = Dispatcher()
 
 
-@dp.message(CommandStart())
-async def cmd_start(message: Message):
-    msg = await bot.send_sticker(chat_id=message.chat.id,
-                                 sticker='CAACAgIAAxkBAAEKQH5k-489IQABlbQqQgTsMWpjmxRyd_kAAi4PAALNvElJfQjcrqE5xpMwBA')
-    time.sleep(3)
-    await bot.delete_message(chat_id=message.chat.id, message_id=msg.message_id)
+async def set_main_menu(bot: Bot):
+    main_menu_commands = [BotCommand(command='/help',
+                                     description='About bot'),
+                          BotCommand(command='/support',
+                                     description='Command for support'),
+                          BotCommand(command='/contacts',
+                                     description='Get contacts'),
+                          BotCommand(command='/payments',
+                                     description='How to pay')]
+
+    await bot.set_my_commands(main_menu_commands)
 
 
 if __name__ == '__main__':
+    dp.startup.register(set_main_menu)
     dp.run_polling(bot)
