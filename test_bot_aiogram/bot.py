051675b40c2981678259866988995f86fc4ebfe5
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-09-10 23:14:49 +0300

    Some bad coding

--- test_bot_aiogram/bot.py
+++ test_bot_aiogram/bot.py
@@ -1,33 +1,23 @@
+import asyncio
+
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
 from aiogram.types import Message
 from config_data.config import load_config
-
-config = load_config('C:/Users/user/PycharmProjects/test_aiogram/test_bot_aiogram/config_data/.env')
-
-API_TOKEN: str = config.tg_bot.token
-
-bot: Bot = Bot(token=API_TOKEN)
-dp: Dispatcher = Dispatcher()
+from keyboards.set_menu import set_main_menu
+from aiogram import Router
 
 
-# handler for command /start
-@dp.message(Command(commands=['start']))
-async def cmd_start(message: Message):
-    await message.answer('Hello! Im the Bot!')
+async def main():
+    config = load_config('C:/Users/user/PycharmProjects/test_aiogram/test_bot_aiogram/config_data/.env')
+    bot: Bot = Bot(token=config.tg_bot.token,
+                   parse_mode='HTML')
+    dp: Dispatcher = Dispatcher()
 
-
-# handler for command /help
-@dp.message(Command(commands=['help']))
-async def cmd_help(message: Message):
-    await message.answer('Send me some and I will echo')
-
-
-# handler for voice message
-@dp.message()
-async def process_any_message(message: Message):
-    await message.answer(message.text)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+    await set_main_menu(bot)
 
 
 if __name__ == '__main__':
-    dp.run_polling(bot)
+    asyncio.run(main())
