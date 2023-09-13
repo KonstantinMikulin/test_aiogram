0b74c2dcec1063ad7374ef066efb7375fcda7463
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-09-13 21:42:14 +0300

    Fixed '/bookmarks'

--- Bookbot/keyboards/bookmarks_kb.py
+++ Bookbot/keyboards/bookmarks_kb.py
@@ -8,7 +8,7 @@ def create_bookmarks_keyboard(*args: int) -> InlineKeyboardMarkup:
     kb_builder = InlineKeyboardBuilder()
 
     for button in sorted(args):
-        kb_builder.row(InlineKeyboardButton(text=f'{button} - {book[:100]}',
+        kb_builder.row(InlineKeyboardButton(text=f'{button} - {book[button][:100]}',
                                             callback_data=str(button)))
         kb_builder.row(InlineKeyboardButton(text=LEXICON['edit_bookmarks_button'],
                                             callback_data='edit_bookmarks'),
@@ -23,7 +23,7 @@ def create_edit_keyboard(*args: int) -> InlineKeyboardMarkup:
     kb_builder = InlineKeyboardBuilder()
 
     for button in sorted(args):
-        kb_builder.row(InlineKeyboardButton(text=f'{LEXICON["del"]} {button} - {book[button[:100]]}',
+        kb_builder.row(InlineKeyboardButton(text=f'{LEXICON["del"]} {button} - {book[button][:100]}',
                                             callback_data=f'{button}del'))
         kb_builder.row(InlineKeyboardButton(text=LEXICON['cancel'],
                                             callback_data='cancel'))
