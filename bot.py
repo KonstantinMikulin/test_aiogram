7d78222122e84103c9e00050bc22a7b3edd29001
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-09-14 19:00:27 +0300

    Some commit

--- bot.py
+++ bot.py
@@ -1,24 +1,48 @@
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
-from aiogram.types import Message
-from keyboards.keyboards import create_inline_kb
-from lexion.lexicon import LEXICON, BUTTONS
+from aiogram.filters.callback_data import CallbackData
+from aiogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 import config
 
-API_TOKEN = config.TOKEN
+BOT_TOKEN = config.TOKEN
 
-bot = Bot(API_TOKEN)
+bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
+
+
+class GoodsCallbackFactory(CallbackData, prefix='goods'):
+    category_id: int
+    subcategory_id: int
+    item_id: int
+
+
+button_1 = InlineKeyboardButton(text='Category 1',
+                                callback_data=GoodsCallbackFactory(
+                                    category_id=1,
+                                    subcategory_id=0,
+                                    item_id=0).pack())
+
+button_2 = InlineKeyboardButton(text='Category 2',
+                                callback_data=GoodsCallbackFactory(
+                                    category_id=1,
+                                    subcategory_id=0,
+                                    item_id=0).pack())
+
+markup = InlineKeyboardMarkup(inline_keyboard=[[button_1], [button_2]])
 
 
 @dp.message(CommandStart())
 async def process_start_cmd(message: Message):
-    keyboard = create_inline_kb(2, **BUTTONS)
-    await message.answer(text='Это инлайн-клавиатура, сформированная функцией '
-                              '<code>create_inline_kb</code>',
-                         reply_markup=keyboard,
-                         parse_mode='HTML')
+    await message.answer(text='This type of keyboard',
+                         reply_markup=markup)
+
+
+@dp.callback_query()
+async def process_any_inline_btn_press(callback: CallbackQuery):
+    print(callback.model_dump_json(indent=4, exclude_none=True))
+
+    await callback.answer()
 
 
 if __name__ == '__main__':
