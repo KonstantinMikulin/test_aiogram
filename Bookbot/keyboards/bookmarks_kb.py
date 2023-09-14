3ee743baa896a5b44e9a75e3f161a75933c56189
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-09-14 16:54:44 +0300

    Problem with bookmarks fixed

--- Bookbot/keyboards/bookmarks_kb.py
+++ Bookbot/keyboards/bookmarks_kb.py
@@ -10,13 +10,13 @@ def create_bookmarks_keyboard(*args: int) -> InlineKeyboardMarkup:
     for button in sorted(args):
         kb_builder.row(InlineKeyboardButton(text=f'{button} - {book[button][:100]}',
                                             callback_data=str(button)))
-        kb_builder.row(InlineKeyboardButton(text=LEXICON['edit_bookmarks_button'],
-                                            callback_data='edit_bookmarks'),
-                       InlineKeyboardButton(text=LEXICON['cancel'],
-                                            callback_data='cancel'),
-                       width=2)
+    kb_builder.row(InlineKeyboardButton(text=LEXICON['edit_bookmarks_button'],
+                                        callback_data='edit_bookmarks'),
+                   InlineKeyboardButton(text=LEXICON['cancel'],
+                                        callback_data='cancel'),
+                   width=2)
 
-        return kb_builder.as_markup()
+    return kb_builder.as_markup()
 
 
 def create_edit_keyboard(*args: int) -> InlineKeyboardMarkup:
