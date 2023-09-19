69480397366ede5b42fd5e3cbfb7b3aa899ae3d9
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-09-19 15:10:40 +0300

    FSM code complete

--- bot_fsm.py
+++ bot_fsm.py
@@ -139,9 +139,9 @@ async def warning_not_gender(message: Message):
 
 # Хэндлер, если отправлено фото и перевод в состояние выбора образования
 @dp.message(StateFilter(FSMFillForm.upload_photo), F.photo[-1].as_('largest_photo'))
-async def process_photo_sent(message: Message, state: FSMContext, largest_photo: PhotoSize)
+async def process_photo_sent(message: Message, state: FSMContext, largest_photo: PhotoSize):
     await state.update_data(
-        photo_unuque_id=largest_photo.file_unique_id,
+        photo_unique_id=largest_photo.file_unique_id,
         photo_id=largest_photo.file_id
     )
     secondary_button = InlineKeyboardButton(
@@ -166,6 +166,7 @@ async def process_photo_sent(message: Message, state: FSMContext, largest_photo:
         text='Спасибо!\n\nУкажите ваше образование',
         reply_markup=markup
     )
+    await state.set_state(FSMFillForm.fill_education)
 
 
 # Хэндлер, если отправлено не фото
@@ -214,41 +215,58 @@ async def warning_not_education(message: Message):
 
 
 # Хэндлер на выбор новостей и вывод из машины состояний
-@dp.
+@dp.callback_query(StateFilter(FSMFillForm.fill_wish_news),
+                   F.data.in_(['yes_news', 'no_news']))
+async def process_wish_news_press(callback: CallbackQuery, state: FSMContext):
+    await state.update_data(wish_news=callback.data == 'yes_news')
+    user_dict[callback.from_user.id] = await state.get_data()
+    await state.clear()
+    await callback.message.edit_text(
+        text='Спасибо! Ваши данные сохранены!\n\n'
+             'Вы вышли из машины состояний'
+    )
+    await callback.message.answer(
+        text='Чтобы посмотреть данные вашей '
+             'анкеты - отправьте команду /showdata'
+    )
 
 
+# Хэндлер на некорректный ответ на согласие по получению новостей
+@dp.message(StateFilter(FSMFillForm.fill_wish_news))
+async def warning_not_wish_news(message: Message):
+    await message.answer(
+        text='Пожалуйста, воспользуйтесь кнопками!\n\n'
+             'Если вы хотите прервать заполнение анкеты - '
+             'отправьте команду /cancel'
+    )
 
 
+# Хендлер для /showdata
+@dp.message(Command(commands=['showdata']), StateFilter(default_state))
+async def process_showdata_cmd(message: Message):
+    if message.from_user.id in user_dict:
+        await message.answer_photo(
+            photo=user_dict[message.from_user.id]['photo_id'],
+            caption=f'Имя: {user_dict[message.from_user.id]["name"]}\n'
+                    f'Возраст: {user_dict[message.from_user.id]["age"]}\n'
+                    f'Пол: {user_dict[message.from_user.id]["gender"]}\n'
+                    f'Образование: {user_dict[message.from_user.id]["education"]}\n'
+                    f'Получать новости: {user_dict[message.from_user.id]["wish_news"]}'
+        )
+    else:
+        await message.answer(
+            text='Вы еще не заполняли анкету. Чтобы приступить - '
+            'отправьте команду /fillform'
+        )
 
 
+# Хэндлер для любых сообщений вне состояний
+@dp.message(StateFilter(default_state))
+async def processs_any_message(message: Message):
+    await message.reply(
+        text='Say what?'
+    )
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    dp.run_polling(bot)
