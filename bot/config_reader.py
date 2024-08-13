ec77ee6ef47fd76a884cc0af70845ee814acfe4d
konstantin_mikulin <mikulin.konstantin@gmail.com>
2024-08-13 13:31:22 +0300

    add config_reader.py

---
+++ bot/config_reader.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+
+from environs import Env
+
+
+@dataclass
+class TgBotConfig:
+    token: str
+    
+    
+@dataclass
+class Config:
+    tg_bot: TgBotConfig
+    
+    
+def load_config(path: str | None=None) -> Config:
+    env = Env()
+    env.read_env(path)
+    
+    return Config(
+        tg_bot=TgBotConfig(
+            token=env('BOT_TOKEN')
+        )
+    )
