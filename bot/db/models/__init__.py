0e7ea0d73ac9c6ffd1bcf0e6661e1d09b219709b
konstantin_mikulin <mikulin.konstantin@gmail.com>
2024-09-27 10:44:02 +0300

    add Score model

--- bot/db/models/__init__.py
+++ bot/db/models/__init__.py
@@ -1,3 +1,4 @@
 from .user import User
+from .user_scores import Score
 
-__all__ = ['User']
+__all__ = ['User', 'Score']
--- bot/db/models/user.py
+++ bot/db/models/user.py
@@ -1,12 +1,14 @@
 from sqlalchemy import BigInteger, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from bot.db import Base
+from bot.db.models.mixins import TimestampMixin
 
-
-class User(Base):
+class User(TimestampMixin, Base):
     __tablename__ = 'users'
     
     telegram_id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
     first_name: Mapped[str] = mapped_column(String, nullable=False)
     last_name: Mapped[str] = mapped_column(String, nullable=True)
+    
+    scores: Mapped[list["Score"]] = relationship(back_populates="user")  # noqa: F821
